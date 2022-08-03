849f6724e01459ff69cdf58fe741f84c7abb14d7
Egwau-Godfrey <egwaujunior@gmail.com>
2022-08-03 17:21:36 +0300

    User Input

---
+++ src/game/game.rs
@@ -0,0 +1,3 @@
+pub fn game() {
+    println!("Hello world");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Enter A Guess : ");
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to take in input");
+    println!("{guess}");
 }
